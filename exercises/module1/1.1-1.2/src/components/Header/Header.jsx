d367b1048d42b0b17b7d09c6dae43daca679fbf4
Youssef ABOUHAMID <youssef.abouhamid@student.vinci.be>
2024-10-02 01:02:10 +0200

    1.1-1.2 : import image

--- exercises/module1/1.1-1.2/src/components/Header/Header.jsx
+++ exercises/module1/1.1-1.2/src/components/Header/Header.jsx
@@ -1,5 +1,12 @@
+import logo from "./LOGO HE VINCI.png";
+
 const Header = (props) => {
-  return (<h1>{props.titre}</h1>);
+  return (
+    <div>
+      <img src={logo} alt="Logo Vinci" />
+      <h1>{props.titre}</h1>);
+    </div>
+  );
 };
 
 export default Header;
