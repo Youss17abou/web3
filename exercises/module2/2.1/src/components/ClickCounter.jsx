a3613613b1ef6ac47aa41f97a353dab56fe8bbb3
Youssef ABOUHAMID <youssef.abouhamid@student.vinci.be>
2024-10-28 15:13:43 +0100

    new:ex2.1-stored

--- exercises/module2/2.1/src/components/ClickCounter.jsx
+++ exercises/module2/2.1/src/components/ClickCounter.jsx
@@ -2,10 +2,12 @@ import { useState } from "react";
 
 // eslint-disable-next-line react/prop-types
 const ClickCounter = ({ title, message }) => {
-  const [count, setCount] = useState(0);
+const [count, setCount] = useState(JSON.parse(localStorage.getItem("count")) || 0);
   const [showMessage, setShowMessage] = useState(false);
   const handleClick = () => {
-    setCount(count + 1);
+    let newCount = count + 1;
+    setCount(newCount);
+    localStorage.setItem("count", JSON.stringify(newCount));
   };
   const handleMouseEnter = () => {
     setShowMessage(true);
