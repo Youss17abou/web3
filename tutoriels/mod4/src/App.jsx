13974ae969f4a14b4644da2aa10129f1844f74dc
Youssef ABOUHAMID <youssef.abouhamid@student.vinci.be>
2024-12-20 22:07:55 +0100

    step: tuto Extracting Communication with the Backend into a Separate Module

--- tutoriels/mod4/src/App.jsx
+++ tutoriels/mod4/src/App.jsx
@@ -2,29 +2,28 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 import Note from "./components/Note";
 
+import noteService from "./services/notes";
+
 const App = () => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note...");
   const [showAll, setShowAll] = useState(true);
 
   useEffect(() => {
-    console.log("effect");
-    axios.get("http://localhost:3001/notes").then((response) => {
-      console.log("promise fulfilled");
-      setNotes(response.data);
+    noteService.getAll().then((initialNotes) => {
+      setNotes(initialNotes);
     });
   }, []);
   console.log("render", notes.length, "notes");
 
-  const toggleImportanceOf = id => {
-    const url = `http://localhost:3001/notes/${id}`
-    const note = notes.find(n => n.id === id)
-    const changedNote = { ...note, important: !note.important }
-  
-    axios.put(url, changedNote).then(response => {
-      setNotes(notes.map(n => n.id === id ? response.data : n))
-    })
-  }
+  const toggleImportanceOf = (id) => {
+    const note = notes.find((n) => n.id === id);
+    const changedNote = { ...note, important: !note.important };
+
+    noteService.update(id, changedNote).then((returnedNote) => {
+      setNotes(notes.map((note) => (note.id === id ? returnedNote : note)));
+    });
+  };
 
   const addNote = (event) => {
     event.preventDefault();
@@ -34,11 +33,10 @@ const App = () => {
       id: notes.length + 1,
     };
 
-    //TODO: Changing the Importance of Notes || finir le tuto et faire l'exercice
-    axios.post("http://localhost:3001/notes", noteObject).then((response) => {
-      console.log(response);
-      setNotes(notes.concat(noteObject));
-      setNewNote("");
+    noteService.create(noteObject)
+    .then(returnedNote => {
+      setNotes(notes.concat(returnedNote))
+      setNewNote('')
     });
   };
 
