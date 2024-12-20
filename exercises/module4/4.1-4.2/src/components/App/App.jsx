51d4925ff7009d380eacccd7731b9a52f5b5e57f
Youssef ABOUHAMID <youssef.abouhamid@student.vinci.be>
2024-12-20 21:34:43 +0100

    Revert "2.11: The Phonebook Step 6"
    This reverts commit a947a4abad50c3223c7237b94e5b0c235305bd9d.

--- exercises/module4/4.1-4.2/src/components/App/App.jsx
+++ exercises/module4/4.1-4.2/src/components/App/App.jsx
@@ -1,42 +1,28 @@
-import { useState, useEffect } from "react";
-import axios from "axios";
+import { useState } from "react";
 
 const App = () => {
-  const [persons, setPersons] = useState([]);
+  const [persons, setPersons] = useState([{ name: "Arto Hellas" , number: "040-123456"}]);
   const [newName, setNewName] = useState("");
   const [newNumber, setNewNumber] = useState("");
 
   const handleNameChange = (event) => {
-    console.log("name :",event.target.value);
-    (event.target.value);
     setNewName(event.target.value);
   };
 
   const handleNumberChange = (event) => {
-    console.log("number :",event.target.value);
     setNewNumber(event.target.value);
   };
 
-  useEffect(() => {
-    console.log("effect");
-    axios.get("http://localhost:3001/persons")
-    .then((response) => {
-      console.log("promise fulfilled");
-      setPersons(response.data);
-    });
-  }, []);   
-
   const addPerson = (event) => {
     event.preventDefault();
+    if (persons.some((person) => person.name === newName)) {
+      alert(`${newName} is already added to phonebook`);
+      return      
+    }
     const personObject = {
       name: newName,
       number: newNumber,
     };
-    if (persons.some((person) => person.name === newName)) {
-      alert(`${newName} is already added to phonebook`);
-      return;
-    }
-
     setPersons(persons.concat(personObject));
     setNewName("");
     setNewNumber("");
@@ -49,9 +35,7 @@ const App = () => {
         <div>
           name: <input value={newName} onChange={handleNameChange} />
         </div>
-        <div>
-          number: <input value={newNumber} onChange={handleNumberChange} />
-        </div>
+        <div>number: <input value={newNumber} onChange={handleNumberChange}/></div>
 
         <div>
           <button type="submit" onClick={addPerson}>
@@ -62,9 +46,7 @@ const App = () => {
       <h2>Numbers</h2>
       <ul>
         {persons.map((person) => (
-          <li key={person.name}>
-            {person.name} {person.number}
-          </li>
+          <li key={person.name}>{person.name} {person.number}</li>
         ))}
       </ul>
     </div>
